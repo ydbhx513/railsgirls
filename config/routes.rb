2f4d20106c9aa004ee90d518bd08d57d4e52191b
ydbhx513 <koyama.tetsu@gmail.com>
2019-08-31 04:17:29 +0900

    bootstrap add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Railsgirls::Application.routes.draw do
+  resources :comments
   get 'pages/info'
   root to: redirect('/ideas')
   resources :ideas do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_143743) do
+ActiveRecord::Schema.define(version: 2019_08_30_115353) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
